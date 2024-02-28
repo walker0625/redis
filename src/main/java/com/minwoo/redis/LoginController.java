ac296bb45f57696153a81dcfc4ab11e6411d9190
전민우 <mw.jeon@lifesemantics.kr>
2024-02-28 16:44:38 +0900

    redis session clustering

---
+++ src/main/java/com/minwoo/redis/LoginController.java
@@ -0,0 +1,30 @@
+package com.minwoo.redis;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+
+@RestController
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login(@RequestParam String name,
+                        HttpSession httpSession) {
+        // application.yml 설정으로 해당 session 정보가 redis에 저장됨
+        httpSession.setAttribute("name", name);
+
+        return "login success";
+    }
+
+    @GetMapping("/name")
+    public String getName(HttpSession httpSession) {
+        // application.yml 설정으로 해당 session 정보를 redis에서 조회함
+        String name = (String) httpSession.getAttribute("name");
+
+        return name;
+    }
+
+}
