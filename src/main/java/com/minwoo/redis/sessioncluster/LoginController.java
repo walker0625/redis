f04f4f81613fce2193bc41498a019e2ee67bb26b
전민우 <mw.jeon@lifesemantics.kr>
2024-02-29 16:03:15 +0900

    레디스 캐시 주석 수정

--- src/main/java/com/minwoo/redis/sessioncluster/LoginController.java
+++ src/main/java/com/minwoo/redis/sessioncluster/LoginController.java
@@ -28,7 +28,8 @@ public class LoginController {
     @GetMapping("/login")
     public String login(@RequestParam String name,
                         HttpSession httpSession) {
-        // redis에 {sessionId : {name : value}} 형태로 저장됨 - application.yml 설정
+        // redis에 {sessionId : {name : value}} 형태로 저장됨 -
+        // redis/session은 application.yml 설정(부트 3.0은 자동 설정)
         httpSession.setAttribute("name", name);
 
         return "login success";
@@ -36,7 +37,12 @@ public class LoginController {
 
     @GetMapping("/name")
     public String getName(HttpSession httpSession) {
-        // redis에 {sessionId : name} 형태로 저장되어 있고
+        // redis에 저장된 attribute value들에서 꺼냄
+        // "sessionId1": {
+        //              "attribute1": "value1",
+        //              "attribute2": "value2",
+        //              ...
+        //            }
         String name = (String) httpSession.getAttribute("name");
 
         return name;
